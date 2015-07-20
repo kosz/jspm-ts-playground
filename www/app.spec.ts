48cb63aca0fc770b5b844a39ec941d59d8710b12
Cosmin <coszmin@gmail.com>
2015-07-20 15:26:55 -0400

    wip(karma+ts): failed attempt at setting up ts in karma

---
+++ www/app.spec.ts
@@ -0,0 +1,9 @@
+/// <reference path="../../../../typings/tsd.d.ts"/>
+
+import {expect} from 'chai';
+
+describe('a typescript test', () => {
+  it('should not throw erros', () => {
+    expect(true).to.be.true;
+  });
+});
